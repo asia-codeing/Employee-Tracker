38b4aede1c2985bad3f8a0ce833096932d412fb0
Asia Alnahi <asia.alius@gmail.com>
2021-04-20 18:05:05 -0700

    Create the inquirer questions

--- employeeTracker.js
+++ employeeTracker.js
@@ -12,6 +12,7 @@ database: 'employee_tracker'
 db.connect((err) =>{
     if (err) throw err;
     //The starting function go here
+    startTracker();
 
 });
 
@@ -22,8 +23,42 @@ const startTracker = () => {
     name: 'action',
     message: 'What would you like to do?',
     choices: [
-        ''
-    ]
+        'View All Employees',
+        'Add Employee',
+        'Update Employee Role',
+        'View All Employees By Department',
+        'View All Employees By Manager',
+        'Update Employee Manager',
+        'Remove Employee'
+    ],
 })
+.then((answer) => {
+    switch (answer.action) {
+        case 'View All Employees' :
+            //function
+            break;
+        case 'Add Employee' :
+            //function
+            break;
+        case 'Update Employee Role' :
+            //function
+            break;
+        case 'View All Employees By Department' :
+            //function
+            break;     
+        case 'View All Employees By Manager' :
+            //function
+            break;
+        case 'Update Employee Manager' :
+            //function
+            break;
+        case 'Remove Employee' :
+            //function
+            break;
+        default:
+            console.log(`Invalid Action: ${answer.action}`);
+            break;
+    }
+});
 
 }
