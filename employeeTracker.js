a5aa5338835fefc7af161ee59a2e8dbad06488f7
Asia Alnahi <asia.alius@gmail.com>
2021-04-23 17:37:15 -0700

    Add (Remove, update, view functions)

--- employeeTracker.js
+++ employeeTracker.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const mysql = require('mysql');
 //Create the connection to the database
 const db = mysql.createConnection({
+multipleStatements: true,
 host:'localhost',
 port: 3306,
 user: 'root',
@@ -24,15 +25,18 @@ const startTracker = () => {
     message: 'What would you like to do?',
     choices: [
         'View All Employees',
+        'View All Employees By Manager',
+        'View All Employees By Department',
         'View Departments',
         'View Roles',
+        'View Total Utilized Budget',
+        'Add Employee',
         'Add Department',
         'Add Role',
-        'Add Employee',
         'Update Employee Role',
-        'View All Employees By Department',
-        'View All Employees By Manager',
         'Update Employee Manager',
+        'Remove Department',
+        'Remove Role',
         'Remove Employee'
     ],
 })
@@ -50,6 +54,10 @@ const startTracker = () => {
             //function
             viewRoles();
             break;
+        case 'View Total Utilized Budget' :
+            //function
+            viewBudget();
+            break;    
         case 'Add Department' :
             //function
             addDepartment();
@@ -62,6 +70,10 @@ const startTracker = () => {
             //function
             addEmployee();
             break;
+        case 'Update Employee Role' :
+            //function
+            updateRole();
+            break;    
         case 'View All Employees By Department' :
             //function
             break;     
@@ -71,9 +83,18 @@ const startTracker = () => {
         case 'Update Employee Manager' :
             //function
             break;
-        case 'Remove Employee' :
+        case 'Remove Department' :
             //function
+            removeDepartment();
+            break;    
+        case 'Remove Role' :
+            //function
+            removeRole();
             break;
+        case 'Remove Employee':
+            //function
+            removeEmployee();
+            break;    
         default:
             console.log(`Invalid Action: ${answer.action}`);
             break;
@@ -81,8 +102,10 @@ const startTracker = () => {
 });
 
 }
-const viewAllEmployees = () => {
 
+//===============================View Functions========================
+
+const viewAllEmployees = () => {
     let query = ('select employee.id,employee.first_name, employee.last_name, role.title, department.name department, role.salary,concat(e.first_name," ",e.last_name) as manager from department inner join role on role.department_id = department.id inner join employee on role.id = employee.role_id left join employee e on employee.manager_id =  e.id ;')
 db.query(query, (err, res) => {
     if(err) throw err;
@@ -108,6 +131,19 @@ const viewRoles = () => {
         startTracker();
       })
 }
+
+const  viewBudget = () => {
+    let query = ('select department.name Department_Name, sum(role.salary) Total_Utilized_Budget from department inner join role on role.department_id = department.id group by department.name;')
+    db.query(query, (err, res) => {
+        if(err) throw err;
+        console.table(res);
+        startTracker();
+      })
+}
+
+
+//=================================Add Functions====================
+
 const addDepartment = () => {
 
     inquirer.prompt({
@@ -150,13 +186,13 @@ const addRole = () => {
         }
 
 ]).then((answer) => {
-        db.query('insert into department set ?',
-        
-        {
+        db.query('insert into role (title, salary, department_id) values ?',[
+            {
             title: answer.title,
             salary: answer.salary,
-            id: answer.departmentId
-        },
+            department_id: answer.departmentId
+            }
+        ],
         (err) => {
             if(err) throw err;
             
@@ -168,107 +204,229 @@ const addRole = () => {
 }
 
 
-
 const addEmployee = () => {
+    const query = ('select title from role; select concat(employee.first_name, employee.last_name) as fullName from employee inner join role on role.id = employee.role_id where role.title like "%Lead%"');
+
+    db.query(query, (err, results) => {
+        if (err) throw err;
 
     inquirer.prompt([
         {
-            type: 'input',
-            name: 'firstname',
-            message: 'Enter employee first name:'
+            type:'input',
+            name: 'firstName',
+            message: "What is the employee's First Name?"
         },
         {
-            type: 'input',
-            name: 'lastname',
-            message: 'Enter employee last name:'
+            type:'input',
+            name: 'lastName',
+            message: "What is the employee's Last Name?"
         },
         {
-            type: 'input',
-            name: 'role_id',
-            message: 'Enter the role id :'
+            type:'list',
+            name: 'role',
+            choices() {
+                const choiceArray = [];
+                results[0].forEach(({ title }) => {
+                    choiceArray.push(title);
+                });
+            return choiceArray;
+          },
+            message: "Select the Job Title:"
         },
         {
-            type: 'input',
-            name: 'manager_id',
-            message: 'Enter the manager id :'
+            type:'list',
+            name: 'manager',
+            choices() {
+                const choiceArray = [];
+                results[1].forEach(({ fullName }) => {
+                  choiceArray.push(fullName);
+                });
+            return choiceArray;
+              },
+            message: "Select Manager Name:"
         }
 
-]).then((answer) => {
-        db.query('insert into department set ?',
-        
-        {
-           fName: answer.firstname,
-            lName: answer.lastname,
-            roleId: answer.role_id,
-            managerId: answer.manager_id
-        },
-        (err) => {
-            if(err) throw err;
-            
-            console.log('New Employee Added!');
-            startTracker();
-        }
+    ]).then((answer) => {
+        db.query(
+            `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES(?, ?, 
+            (SELECT id FROM role WHERE title = ? ), 
+            (SELECT id FROM (SELECT id FROM employee WHERE CONCAT(first_name," ",last_name) = ? ) AS manager))`, [answer.firstName, answer.lastName, answer.role, answer.manager]
         )
-    } )
+        startTracker();
+    })
+ });
+}
+
+//=============================Update Functions====================
+const updateRole = () => {
+  const query = ('select concat(employee.first_name, employee.last_name) as fullName from employee inner join role on role.id = employee.role_id; select title from role');
+  db.query(query, (err, results) => {
+      if(err) throw err;
+
+      inquirer.prompt([
+          {
+              type: 'list',
+              name: 'employeeName',
+              choices() {
+                const choiceArray = [];
+                results[0].forEach(({ fullName }) => {
+                  choiceArray.push(fullName);
+                });
+            return choiceArray;
+              },
+              message: 'Select Employee name to update role:'
+
+          },
+          {
+            type:'list',
+            name: 'newRole',
+            choices() {
+                const choiceArray = [];
+                results[1].forEach(({ title }) => {
+                    choiceArray.push(title);
+                });
+            return choiceArray;
+          },
+            message: "Select the Job Title:" 
+          }
+      ])
+      .then((answer) => {
+          db.query(`UPDATE employee 
+          SET role_id = (SELECT id FROM role WHERE title = ? ) 
+          WHERE id = (SELECT id FROM(SELECT id FROM employee WHERE CONCAT(first_name," ",last_name) = ?) AS fullName)`,
+            [
+                {
+                    updated:answer.newRole,
+                }, 
+                {
+                    employee:answer.employeeName,
+                }
+                
+            ], (err, results) => {
+                    if (err) throw err;
+                    console.log(results + 'Role Updated!');
+                    startTracker();
+                });
+        });
+    });
+
+}
+//===========================Remove Functions=================
+const removeDepartment = () => {
+    query = ('select * from department');
+    db.query(query, (err, results) => {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                name: 'removeDepartment',
+                type: 'list',
+                choices() {
+                    const choiceArray = [];
+                    results.forEach(({ name }) => {
+                        choiceArray.push(name);
+                    });
+                return choiceArray;
+              },
+                message: 'Select anDepartment to remove:'
+            }
+        ]).then((answer) => {
+            db.query(`delete from department where ? `, 
+            { 
+                name: answer.removeDepartment
+            },
+            (err, results) => {
+                if (err) throw err;
+                console.log(results + 'Department Removed');
+                startTracker();
+            });
+
+        })
+
+    })
+
+}
+
+const removeRole = () => {
+    query = ('select * from role');
+    db.query(query, (err, results) => {
+        if (err) throw err;
+
+        inquirer.prompt([
+            {
+                name: 'removeRole',
+                type: 'list',
+                choices() {
+                    const choiceArray = [];
+                    results.forEach(({ title }) => {
+                        choiceArray.push(title);
+                    });
+                return choiceArray;
+              },
+                message: 'Select a Role to remove:'
+            }
+        ]).then((answer) => {
+            db.query(`delete from role where ? `, { title: answer.removeRole},
+            (err, results) => {
+                if (err) throw err;
+                console.log(results + 'Role Removed');
+                startTracker();
+            });
+        })
+
+    })
+
 }
 
 
+const removeEmployee = () => {
+    const query =('select employee.id,employee.first_name, employee.last_name, role.title, department.name department, role.salary,concat(e.first_name," ",e.last_name) as manager from department inner join role on role.department_id = department.id inner join employee on role.id = employee.role_id left join employee e on employee.manager_id =  e.id ;')
+    db.query(query, (err, results) => {
+        if (err) throw err;
+        console.log(' ');
+        console.table(results)
+        inquirer.prompt([
+            {
+                name: 'IDtoRemove',
+                type: 'input',
+                message: 'Enter the ID of the Employee to remove:'
+            }
+        ]).then((answer) => {
+            db.query(`DELETE FROM employee where ?`, { id: answer.IDtoRemove })
+            startTracker();
+        })
+    })
+}
 
 
-// const roleArr = [];
-// const managersArr = []
+// const removeEmployee = () => {
+//     query = ('select id, concat(first_name," ",last_name) as employee_name from employee;');
+//     db.query(query, (err, results) => {
+//         if (err) throw err;
 
-// const addEmployee = () => {
-    
-//     inquirer.prompt([
-//         {
-//             type:'input',
-//             name: 'firstName',
-//             message: "What is the employee's First Name?"
-//         },
-//         {
-//             type:'input',
-//             name: 'lastName',
-//             message: "What is the employee's Last Name?"
-//         },
-//         {
-//             type:'list',
-//             name: 'role',
-//             choices:function () {
-//                 let query = ('select id, title from role;')
-//             db.query(query, (err, res) => {
-//                 if(err) throw err;
-//             // console.table(res);
-//             for (let i = 0; i < res.length; i++){
-//                 roleArr.push(res[1].title)
+//         inquirer.prompt([
+//             {
+//                 name: 'removeEmployee',
+//                 type: 'list',
+//                 choices() {
+//                     const choiceArray = [];
+//                     results.forEach(({ employee_name }) => {
+//                         choiceArray.push(employee_name);
+//                     });
+//                 return choiceArray;
+//               },
+//                 message: 'Select a employee name to remove:'
 //             }
-//             })
-//             },
-//             message: "Select the Job Title:"
-//         },
-//         {
-//             type:'list',
-//             name: 'manager',
-//             choices: function () {
-//                 let query = ('select id, concat(first_name," ",last_name) as manager from employee where manager_id is null;');
-//                     db.query(query, (err, res) => {
-//                         if(err) throw err;
-//                        // console.table(res);
-//                        for (let i = 0; i < res.length; i++) {
-//                         managersArr.push(res[i].first_name);
-//                        }
-//                     })
-//             },
-//             message: "Select Manager Name:"
-//         }
+//         ]).then((answer) => {
+//             db.query(`delete from employee where (concat(first_name," ",last_name) as employee_name from employee) = ${answer.removeEmployee}`,(err, results) => {
+//                 if (err) throw err;
+//                 console.log(results + 'Employee Removed');
+//                 startTracker();
+//             });
+//         })
 
-//     ]).then((answer) => {
-//         db.query(
-//             `INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES(?, ?`, 
-           
-//             [answer.firstName, answer.lastName, answer.role, answer.manager]
-//         )
-//         startTracker();
 //     })
+
 // }
+
 
